bdd732b86864b1d33000a00bbcb312d884498368
Rizki Susilo Ramadhan <rizkysr90@gmail.com>
2022-12-31 01:55:55 +0700

    createion of landing page

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -6,8 +6,19 @@
   @vite('resources/css/app.css')
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="bg-neutral min-h-screen w-full md:w-2/6 mx-auto flex justify-center items-center">
+    <div class="mockup-window border bg-base-300 w-full mx-4">
+      <div class="flex flex-col items-center px-4 py-16 bg-base-200">
+        <p>Hello World!</p>
+        <div class="">Tugas UAS Business Web Solution</div>
+        <div class="font-bold">"EZCatalog"</div>
+        <div class="mt-4">
+          <a href="/users/register" class="btn btn-primary mx-2">Daftar</a>
+          <a href="/users/login" class="btn btn-secondary mx-2">Login</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </body>
 </html>
